5bcabcfac6c6cec2c689512acb6fbb49a2c10612
NOOB2033 <zaza322228@mail.ru>
2022-10-20 21:12:35 +0700

    lab5

---
+++ lab5/5.js
@@ -0,0 +1,11 @@
+function open_(id) {
+    document.getElementById(id).style.display='block';
+    document.getElementById(0).style.display='block';
+}
+
+function close_() {
+    document.getElementById(0).style.display='none';
+    document.getElementById(1).style.display='none';
+    document.getElementById(2).style.display='none';
+    document.getElementById(3).style.display='none';
+}
